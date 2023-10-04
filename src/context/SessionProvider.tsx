7dd052d1fac819ace74e746bc9d425bdfafd6f9d
VictorJThomas <112771636+VictorJThomas@users.noreply.github.com>
2023-10-04 13:21:06 -0400

    SessionProvider update

--- src/context/SessionProvider.tsx
+++ src/context/SessionProvider.tsx
@@ -2,7 +2,7 @@
 
 import { SessionProvider } from "next-auth/react"
 
-export default function Provider({ children, session}: {children: React.ReactNode}) {
+export default function Provider({ children}: {children: React.ReactNode}) {
     return (
         <SessionProvider>
             {children}
