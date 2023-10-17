b63d42f24e01960eb917eb852a934ea212e63a01
JhonMedina <jhon-43769@outlook.com>
2023-10-17 15:46:27 -0400

    add chat bot

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,21 @@
-const ChatBotPage = () => {
-    return (
-        <div>ChatBot Page</div>
-    )
-}
+import {ChatWindow} from '@/components/ChatWindow'
 
-export default ChatBotPage
+export default function ChatBotPage() {
+    const InfoCard = (
+      <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden">
+        <h1 className="text-3xl md:text-4xl mb-4">
+          ▲ Next.js + LangChain.js 🦜 Por que a vitol le dio la gana el langChain🔗
+        </h1>
+  
+      </div>
+    );
+    return (
+      <ChatWindow
+        endpoint="api/chat"
+        emoji="🏴‍☠️"
+        titleText="Patchy the Chatty Pirate"
+        placeholder="De que quiere hablar Miop?"
+        emptyStateComponent={InfoCard}
+      ></ChatWindow>
+    );
+  }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { usePathname } from 'next/navigation';
+
+export function Navbar() {
+  const pathname = usePathname();
+  return (
+    <nav className="mb-4">
+
+    </nav>
+  );
+}
