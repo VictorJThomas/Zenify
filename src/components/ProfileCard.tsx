9669178e77dded8bd8aba0e45cbd35d074c2c125
VictorJThomas <112771636+VictorJThomas@users.noreply.github.com>
2023-10-20 16:01:03 -0400

    ProfileCard component

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -10,7 +10,7 @@ const ProfileCard = () => {
     return (
         <div className="w-full text-center pb-8">
             <div className="pl-10">
-                <Image className="bg-zinc-200  rounded-lg" src={userImage} alt={`${session?.user?.email} image`} width={100} height={100}/>
+                <Image priority className="bg-zinc-200  rounded-lg" src={userImage} alt={`${session?.user?.email} image`} width={100} height={100}/>
             </div>
             <div className="text-base font-medium pr-8 pt-6">{session?.user?.name}</div>
         </div>
