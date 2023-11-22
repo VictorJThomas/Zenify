2da1dab403e397f561bde2057e24499fdcd96404
VictorJThomas <112771636+VictorJThomas@users.noreply.github.com>
2023-11-22 10:34:24 -0400

    Posts seed created

--- src/app/dashboard/post/page.tsx
+++ src/app/dashboard/post/page.tsx
@@ -1,8 +1,22 @@
+import { prisma } from "@/utils/prisma";
 import MainSection from "./components/MainSection";
 import SecondSection from "./components/SecondSection";
 import Tags from "./components/Tags";
+import { Post } from "@prisma/client";
 
-const PostPage = () => {
+const getPosts = async () => {
+  const posts: Array<Post> = await prisma.post.findMany()
+
+  return posts
+}
+
+const PostPage = async () => {
+  const posts = await getPosts();
+
+  const formatPosts = () => {
+    
+  }
+
   return (
     <div className="w-full px-10 max-h-[50%]">
       <h1 className="text-6xl">Welcome!</h1>
